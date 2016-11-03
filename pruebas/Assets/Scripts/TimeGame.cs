a11d49ee33f2e77e6a967779ba87c2f206b506fc
Treidd <treidder@gmail.com>
2016-11-03 20:52:34 +0100

    Test PlayerWaitTime

---
+++ pruebas/Assets/Scripts/TimeGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeGame : MonoBehaviour {
+
+    public float roundStartTime;
+    public float playerWaitTime;
+    int waitTime;
+	void Start () {
+        print("Press the space bar once you think the alloted time is up");
+        SetNewRandomTime();
+       
+    }
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            playerWaitTime = Time.time - roundStartTime;
+            Debug.Log("Tiempo del player: " + playerWaitTime);
+            float error = Mathf.Abs(waitTime - playerWaitTime);
+            print("You waited for" + playerWaitTime + " seconds. That's " + error + " seconds off.");
+            SetNewRandomTime();
+        }
+	}
+    void SetNewRandomTime()
+	{
+        waitTime = Random.Range(0,60);
+        roundStartTime = Time.time;
+        print(waitTime + " seconds.");
+     
+    }
+}
---
+++ pruebas/Assets/Scripts/mathfabs.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mathfabs : MonoBehaviour {
+
+    public float maRan;
+    private float resul;
+
+	void Start () {
+       
+    }
+
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Randi();
+        }
+	}
+
+    void Randi()
+    {
+        maRan = Random.Range(-10, 3);
+        resul = Mathf.Abs(maRan);
+        print("Resultado random: " + maRan);
+        print("Mathf.Abs: " + resul);
+    }
+}
